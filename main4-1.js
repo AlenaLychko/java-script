e460beeef89a0865fa00dab5ff5d5bac1d8777cd
Alena Lychko <alenaxxprv@gmail.com>
2025-07-20 13:06:49 -0400

    s

---
+++ main4-1.js
@@ -0,0 +1,46 @@
+const num = 0;
+console.log(num >= 0 || num <= 17);
+//&& лошічне і те і те і те
+// || логічне або
+// !логічне не
+console.log(!false);
+
+//false && виконуватись не буде
+//true || виконуватись не буде
+
+//if(умова){
+//   дія
+//}else{
+//   дія
+//}
+
+//const isRain = true;
+//if(isRain===true){
+///  console.log("візьми парасольку");
+//}else{
+//  console.log("НЕ бери парасольку!");
+//}
+
+//const price = 6000;
+
+//if (price < 1000 && price > 550) {
+// console.log("візьми квиток на економ");
+//} else if (price >= 1000 && price < 1500) {
+//console.log("візьми квиток на середний класс");
+//} else if (price >= 1500) {
+//console.log("лети бізнес класом");
+//} else {
+// console.log("тобі не вистачае грошей!");
+//}
+
+//if(age > 18){
+//console.log("ти перволітній");
+//}
+
+    // const isRain = true? 'іде дощь' : 'дощу нема'
+    // console.log(isRain);
+
+    const salary = 4000;
+
+ const isRich = salary >= 2500? "ти багатий" : "іди працюй"
+ console.log(isRich);
