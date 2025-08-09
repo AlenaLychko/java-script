303db5431d75a7728eae3f74d7af6527a1d9fe6e
Alena Lychko <alenaxxprv@gmail.com>
2025-08-09 12:09:52 -0400

    s

---
+++ main5-2.js
@@ -0,0 +1,40 @@
+// while(умова у дужках тру) {
+//     буде виконуватись ця дія
+// }
+
+//  let age = 0;
+// // while(age < 18){
+// //     console.log('ти ще дитина',age);
+// //     age += 1;
+// // }
+
+// do{
+//     console.log('ти ще дитина тобі',age);
+//     age += 1;
+// }while(age>18)
+
+// let password =''
+// do{
+//     password = prompt('введи пароль довше 4 символи');
+// } while (password.length<=4)
+//     console.log('ти ввів', password);
+
+// for (let index = 0; index <= 100; index += 5) {
+//   console.log(`index ${index}`);
+// }
+
+//порахувати сумму чисел від 1 до 100
+
+// let sum = 0;
+// for(let i =1; i <= 100; i +=1){
+//     sum = sum + i;
+    
+// }
+// console.log(sum);
+
+let k = 0;
+for(let i =0; i <= 90; i +=9){
+    k+=1
+   console.log(`9*${k} =${i}`)
+    
+}
