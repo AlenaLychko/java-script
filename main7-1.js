c96505d3e7b4792126bbeaf7d213dcf93db83d57
Alena Lychko <alenaxxprv@gmail.com>
2025-08-16 13:23:12 -0400

    і

---
+++ main7-1.js
@@ -0,0 +1,61 @@
+// const friuts = ["apple", "pineaple", "banana", "potato"];
+
+// console.log(friuts.lenght);
+// console.log(friuts[3]);
+
+// //  for(let i=0; i<friuts.lenght; i=+1){
+// //     console.log(fruits[i]);
+// //  }
+
+// const vegiatable = "potato";
+
+// for (let fruit of friuts) {
+//   if (fruit === vegiatable) {
+//     console.log("який жах це овочь");
+//   } else {
+//     console.log("це фрукт");
+//   }
+// }
+
+// const lists = [5, 15, 17, 77, 110];
+
+// console.log(lists[2])
+
+// for (let i = 0; i < lists.length; i = i + 1) {
+//   let summ = 0;
+//   summ = summ + lists[i];
+// }
+// console.log(summ);
+
+// const matrix = [
+//   [1, 1, 1],
+//   [2, 2, 2],
+//   [3, 3, 3],
+// ];
+// for (let i = 0; i < matrix.length; i = i + 1) {
+//   for (let j = 0; j < matrix[i].length; j = j + 1) {
+//     console.log(matrix[i][j]);
+//   }
+// }
+// console.log(matrix[2][1]);
+
+// const lists1 = [5, 15, 17, 77, 110];
+
+// const lists2 = [15, 115, 117, 177, 1110];
+
+// for (let list2 of lists2) {
+//   lists1.push(list2);
+// }
+// console.log(lists2);
+// lists2[4] = 10;
+// console.log(lists2)
+
+const loggins = ["pa_lin14", "Anna56ui", "a_j_moody", "alenaassx"];
+
+for (let loggin of loggins) {
+  if (loggin === "alenaassx") {
+    console.log("this is your loggin");
+  } else {
+    console.log("this is not you");
+  }
+}
