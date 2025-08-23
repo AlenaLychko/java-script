1eb9f01d663883fa7cfbd1b52f260eaff185f0e6
Alena Lychko <alenaxxprv@gmail.com>
2025-08-23 13:17:56 -0400

    sd

---
+++ main8-1.js
@@ -0,0 +1,60 @@
+// const fruits = ["apple", "pineapple", "banana", "potato"];
+// const veg = ["cucumber", "watermelon", "melon"];
+// const summ = fruits.concat(veg);
+
+// const fr = friuts.slice(1,4); //створює з масиву підмасив
+//  (виділяє з нього частинку) (інд початку, інд кінець)
+// const fr = friuts.slice(-2) мінус вказує що елемент рахують с кіньця
+
+// const fr = friuts.splice(1,2) //повертая масив  з вдаленими елементами
+//(індекс первого числа на видалення, кількість числе які ми видалимо)
+
+//  friuts.splice(1, 0,'cucumber', 'watermelon','melon') //додати нові елементі не видиляючи
+// інші (індекс куди додаємо, 0 (шоб нічого не видаляти), через кому які підставляемо
+
+//  friuts.splice(1, 2, "cucumber", "watermelon", "melon"); // заміняє певні елементи масива
+//(індекс куди додаємо , есть яку видиля\мо, через кому елемент які представляємо)
+
+//fruits.splice(1, 2, 'cucumber', 'watermelon', 'melon')//заміняє певні елементи масива
+//(індекст куди додаємо, к-сть яку видаляємо, через кому елементи які підставляємо)
+
+// console.log(summ);
+
+// практика
+
+//  const students = ["Поліна", "олександра", "влад", "алена"];
+
+// console.log(students.includes("алена"));
+// console.log(students.indexOf("алена"));
+
+// const cut = students.slice(1, 3);
+// console.log(cut);
+
+//  const films = ["Interstellar", "Harry Potter", "Watermelon adventure"];
+// const text = films.join("❤");
+// console.log(text);
+
+// films.push("Marli and me");
+// // films.splice(
+// //   1,
+// //   1,
+// //   "Harry Potter 1",
+// //   "Harry Potter 2",
+// //   "Harry Potter 3",
+// //   "Harry Potter 4"
+// // );
+// // console.log(films);
+
+// const Harrys = [
+//   "Harry Potter 1",
+//   "Harry Potter 2",
+//   "Harry Potter 3",
+//   "Harry Potter 4",
+//   "Harry Poter 5",
+// ];
+
+// films.splice(1, 1);
+// console.log(films);
+
+// const plus = films.concat(Harrys);
+// console.log(plus);
