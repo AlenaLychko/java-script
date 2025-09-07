d1921e565d1fc6cbe611d20f65c1949128f32ccd
Alena Lychko <alenaxxprv@gmail.com>
2025-09-07 13:22:39 -0400

    s

---
+++ main9-2.js
@@ -0,0 +1,63 @@
+// //об'єкт
+// const lesson = {
+//   nameLesson: "Math",
+//   tutor: "Klavdia Petrivna",
+//   duration: 45,
+//   students: ["Vlad", "Sasha", "Vadym", "Anna", "Polina", "Olena"],
+// };
+
+// lesson.tutor = "Taras Shevchenko"; //міняємо знасення об'єкта за ключем
+// lesson.audience = "101, 3 floor";
+
+// delete lesson.duration //delete об'єкт.key deleting
+
+// console.log(lesson["students"]);
+
+// let tutor = "Klavdia Petrivna";
+// const lesson = {
+//   nameLesson: "Math",
+//   tutor,
+//   duration: 45,
+//   students: ["Vlad", "Sasha", "Sasha", "Vadym", "Anna", "Polina", "Olena"],
+//   fu() {
+//     console.log(this.nameLesson);
+//   },
+// };
+// console.log(lesson.fu());
+
+// const characters [{
+//   name: "Harry Potter",
+//   house: "Gryffindor",
+//   wand: {
+//     wood: "глід",
+//     core: "перо фенікса",
+//     lenght: "11 inch",
+//   },
+//   friends: ["Ronald", "Heriona", "Hagrid", "Dobby"],
+//   abilities: "Parseltong",
+//   spells: ["expecto patronum", "expeliarmus"],
+//   introduce() {
+//     return `hello i am Harry Poter ${this.name}, i am from ${
+//        this.house}
+//     }`;
+//   },
+
+
+// {
+//   name: "Hermione Granger",
+//   house: "Gryffindor",
+//   wand: {
+//     wood: "vine",
+//     core: "dragon heartstring",
+//     length: "10 inches",
+//   },
+//   friends: ["Ron Weasley", "Neville Longbottom", "Ginny Weasley", "Poter"],
+//   abilities: "Brilliant memory",
+//   spells: ["Alohomora", "Protego", "Petrificus Totalus"],
+//   introduce() {
+//     return `hello i am Hermione Granger ${this.name}, i am from ${this.house}
+//     }`;
+//   },
+// };]
+
+
