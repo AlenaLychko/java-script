0244706876c8a1835d8434c20c9910db3fa747c3
Alena Lychko <alenaxxprv@gmail.com>
2025-09-27 13:29:22 -0400

    j

--- main11.js
+++ main11.js
@@ -64,21 +64,118 @@
 
 // console.log( rest,size)
 
-const car = {
-  make: "Toyota",
-  model: "Camry",
-  year: 2020,
-  features: ["power windows rear camera", "navigation"],
-  safety: {
-    airbags: true,
-    antilock_brakes: true,
-    stability_control: true,
+// const car = {
+//   make: "Toyota",
+//   model: "Camry",
+//   year: 2020,
+//   features: ["power windows rear camera", "navigation"],
+//   safety: {
+//     airbags: true,
+//     antilock_brakes: true,
+//     stability_control: true,
+//   },
+// };
+// const {
+//   make,
+//   model,
+//   year,
+//   features: [features2, features3],
+//   safety: { airbags, antilock_brakes, stability_control },
+// } = car;
+
+// let arr = ["Ilya", "Kantor"];
+// [item1, item2] = arr;
+// console.log(item2);
+
+// const person = {
+//   name: "Nelli",
+//   surname: "Laroy",
+//   age: 25,
+// };
+// const { name, surname, age } = person;
+// console.log(surname);
+
+// const student = {
+//   name: "Bruce",
+//   surname: "Lee",
+//   grades: [4, 5, 3],
+// };
+// const calculateAverageGrade = function ({ name, surname, grades }) {
+//   let avarage;
+//   for (const i of grades) {
+//     avarage += i;
+//   }
+//   return avarage/grades.lenght
+// };
+// console.log(calculateAverageGrade(student));
+
+// const items = [
+//   { name: "Футболка", price: 250, quantity: 2 },
+//   { name: "Джинси", price: 800, quantity: 1 },
+//   { name: "Кросівки", price: 1200, quantity: 1 },
+// ];
+// const calculateTotal = function (obj) {
+//   let total = 0;
+// for(const {price, quantity} of obj){
+// total += price*quantity
+// }
+
+//   return total
+// };
+// console.log(calculateTotalCost(items));
+
+// const dishes = [
+//   { name: "Салат Цезар", calories: 250, portions: 2 },
+//   { name: "Паста Болоньєзе", calories: 600, portions: 3 },
+//   { name: "Суп", calories: 150, portions: 1 },
+// ];
+
+// const calculateAverageCalories = function (col) {
+//   let total = 0;
+//   for ({ calories, portions } of col) {
+//     total += calories * portions;
+//   }
+//   return total;
+// };
+// console.log(calculateAverageCalories(dishes));
+
+// const data = {
+//   users: [
+//     { name: "John", age: 25 },
+//     { name: "Jane", age: 17 },
+//     { name: "Bob", age: 30 },
+//     { name: "Alice", age: 20 },
+//   ],
+// };
+// const getAdultUsers = function ({users}) {
+//   const adults =[
+
+//   ]
+//   for(const {name,age} of users){
+//     if(age>18){
+//       adults.push({name,age})
+//     }
+//   }
+//   return adults
+// };
+// console.log(getAdultUsers(data));
+
+const library = {
+  books: [],
+  lastId: 0,
+  createBook(title, author) {
+    return {
+      id: ++this.lastId,
+      title,
+      author,
+    };
   },
+  addBook(title, author) {
+    const newBooks = this.createBook.apply(title, author);
+    this.books.push(newBoooks);
+    alert(`book${title} added to library`)
+  },
+  removeBook(id){
+    
+  }
 };
-const {
-  make,
-  model,
-  year,
-  features: [features2, features3],
-  safety: { airbags, antilock_brakes, stability_control },
-} = car;
