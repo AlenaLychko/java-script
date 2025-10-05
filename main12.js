8dbfdf22396c850d47ddc4eb996cfa78a2e43c57
Alena Lychko <alenaxxprv@gmail.com>
2025-10-05 12:55:23 -0400

    d

--- main12.js
+++ main12.js
@@ -64,3 +64,42 @@
 // const summ = arr.reduce((acc, val)=> acc+val, 1000000)
 // console.log(summ)
 
+// const tweets = [
+//   { id: "000", likes: 5, tags: ["js", "nodejs"] },
+//   { id: "001", likes: 2, tags: ["html", "css"] },
+//   { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+//   { id: "003", likes: 8, tags: ["css", "react"] },
+//   { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
+// ];
+// // const summ = tweets.reduce((acc, val)=> acc+val.likes, 0)
+// // console.log(summ)
+
+// // const summLikes = function(array){
+// //     return array.reduce((accTweet, valTweet) => accTweet + valTweet.likes, 0);
+// // }
+// // console.log(summLikes(tweets))
+
+// const tagsArray = tweets.reduce((newArray, tweet) => {
+//     newArray.push(...tweet.tags)
+// return newArrray},[])
+// console.log(tagsArray)
+
+const players = [
+  { id: "player-1", name: "Mango", timePlayed: 310, points: 54, online: false },
+  { id: "player-2", name: "Poly", timePlayed: 470, points: 92, online: true },
+  { id: "player-3", name: "Kiwi", timePlayed: 230, points: 48, online: true },
+  { id: "player-4", name: "Ajax", timePlayed: 150, points: 71, online: false },
+  { id: "player-5", name: "Chelsy", timePlayed: 80, points: 48, online: true },
+];
+
+// const summ = players.reduce((acc, val) => acc + val.timePlayed, 0);
+// console.log(summ);
+
+const totalTime = function (array) {
+  return array.reduce(
+    (accPlayers, valPlayers) => accPlayers + valPlayers.timePlayed,
+    0
+  );
+};
+ console.log(totalTime(players));
+
