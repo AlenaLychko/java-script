9aa7b3f9b0be60e9fbc3a315a24c539d728cab41
Alena Lychko <alenaxxprv@gmail.com>
2025-11-01 12:04:49 -0400

    f

--- main16.js
+++ main16.js
@@ -15,12 +15,40 @@
 // mainTitle.style.color = "blue";
 // mainTitle.classList.add('text-accent')
 
-const list = document.querySelector(".site-nav");
-const link = document.querySelector(".site-nav__item a ");
-link.classList.add("site-nav__link");
-console.log(link);
-list.style.backgroundColor = "red";
+// const list = document.querySelector(".site-nav");
+// const link = document.querySelector(".site-nav__item a ");
+// link.classList.add("site-nav__link");
+// console.log(link);
+// list.style.backgroundColor = "red";
 
-const linkHref = document.querySelector('.site-nav__link a[href="https:/"]');
-linkHref.classList.add("big-font");
-console.log(linkHref);
+// const linkHref = document.querySelector('.site-nav__link a[href="https:/"]');
+// linkHref.classList.add("big-font");
+// console.log(linkHref);
+
+// const lists = document.querySelectorAll("#list a");
+// // lists.forEach((link) => {
+// //   const href = link.getAttribute("href");
+
+// //   if (link.href.startsWith("http://")) {
+// //     link.style.color = "orange";
+// //   }
+// // });
+
+// lists[0].classList.add("text-accent");
+// lists[(lists.length = 1)].classList.add("text-accent");
+
+// const title = document.querySelector("h1");
+// title.textContent = ('coding')
+// title.classList.add('text-acccent')
+
+const table = document.querySelector("#age-table");
+console.log(table);
+
+const labels = document.querySelectorAll("label");
+console.log(labels);
+
+const search = document.querySelector('form[name="search-person"]');
+console.log(search);
+
+const inputs = search.querySelectorAll("input");
+console.log(inputs[inputs.length - 1]);
